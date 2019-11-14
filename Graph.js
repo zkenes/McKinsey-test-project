8b6db579dd81605e5dbe8a7c79ec4ad7c56e18d8
Zhanserik <zhans.kenes@gmail.com>
2019-11-14 20:54:57 +0600

    feat: add babel

--- Graph.js
+++ Graph.js
@@ -9,6 +9,25 @@ export class Graph {
         const edge = new Edge(destNode, weight);
         this.adjacencyMap.get(sourceNode.id).push(edge);
     }
+    printGraph() {
+        var keys = this.adjacencyMap.keys();
+
+        // iterate over the vertices
+        for (var key of keys) {
+            // great the corresponding adjacency list
+            // for the vertex
+            var nodes = this.adjacencyMap.get(key).map(e => e.destNode.id);
+            var conc = "";
+
+            // iterate over the adjacency list
+            // concatenate the values into a string
+            for (var j of nodes)
+                conc += j + " ";
+
+            // print the vertex and its adjacency list
+            console.log(i + " -> " + conc);
+        }
+    }
 }
 export class Node {
     constructor(id) {
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-import { Graph, Node, Edge } from './Graph';
+// import { Graph, Node, Edge } from './Graph';
+const graph = require('./Graph');
+
+const node1 = new graph.Node("A");
+const node2 = new graph.Node("B");
+const node3 = new graph.Node("C");
+const node4 = new graph.Node("D");
+const node5 = new graph.Node("F");
+const node6 = new graph.Node("G");
